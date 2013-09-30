266741ae3ca8719933ac98123dd2b255c929c25e
Joe Marshall <joseph.charles.marshall@gmail.com>
2013-09-30 14:42:02 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LionWitch::Application.config.secret_key_base = '68fa593aac3c4f57cf16fb6c8428be8ce693d84f578dab7d6202246435b7609d923ef222d705e75868ab96799ce6fcc97850c3a5f9fa5c466aa68a14516d44ef'
